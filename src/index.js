a13321fdb00748a46898f0806e18ba8d2a54c066
mayorr-star <immaboat11@gmail.com>
2024-06-08 13:51:06 +0000

    Tested the various linkdList methods

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-ECHO is on.
+const LinkedList = require("./modules/linked_List/linkedList");
+const list = new LinkedList()
+
+list.append(1)
+list.append(2)
+list.append(3)
+list.append(4)
+list.append(5)
+console.log(list)
+console.log(list.toString())
